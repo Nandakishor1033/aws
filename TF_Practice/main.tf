09e1aa338cf998982bb3159e283ac98baaf66298
Nandakishor1033 <ncg.1033@gmail.com>
2024-03-31 15:57:38 -0500

    provider added

--- TF_Practice/main.tf
+++ TF_Practice/main.tf
@@ -4,6 +4,16 @@ provider "aws" {
   region = "us-west-2" # Update with your desired AWS region
 }
 
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.43.0"
+    }
+  }
+}
+
 resource "aws_instance" "ec2" {
   ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI ID, update as needed
   instance_type = "t2.micro"               # Instance type, update as needed
